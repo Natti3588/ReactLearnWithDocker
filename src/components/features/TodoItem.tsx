5b6867a873249cdff38450631205a281d15f74ed
Taisei2 <tnbox3588git@gmail.com>
2026-04-11 16:36:15 +0900

    refactor: TodoItemから不要なDaisyUIテンプレートを削除

--- src/components/features/TodoItem.tsx
+++ src/components/features/TodoItem.tsx
@@ -2,14 +2,7 @@ const TodoItem: React.FC = () => {
   return (
     <li className="list-row">
       <div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
-      <div>
-        <img
-          className="size-10 rounded-box"
-          src="https://img.daisyui.com/images/profile/demo/1@94.webp"
-        />
-      </div>
       <div className="list-col-grow">
-        <div>Dio Lupa</div>
         <div className="text-xs uppercase font-semibold opacity-60">
           Remaining Reason
         </div>
