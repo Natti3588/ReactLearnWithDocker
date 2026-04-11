671e3cc8b428b93dc1f8f659e9b74f91f7b963e9
Taisei2 <tnbox3588git@gmail.com>
2026-04-11 16:33:42 +0900

    style: TodoList一覧のテキストを太字に変更

--- src/components/features/TodoList.tsx
+++ src/components/features/TodoList.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
+import TodoItem from "./TodoItem";
 
 const TodoList: React.FC = () => {
   const [todos, setTodos] = useState("");
 
   return (
     <ul className="list bg-base-100 rounded-box shadow-md mx-10">
-      <li className="p-4 pb-2 text-xs opacity-60 tracking-wide text-center">
+      <li className="p-4 pb-2 text-lg opacity-60 tracking-wide text-center uppercase">
         TodoList一覧
       </li>
-
-      
+      <TodoItem />
     </ul>
   );
 };
