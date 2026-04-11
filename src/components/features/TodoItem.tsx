44f4b0eb67de4bbd4f7cc5bae844b079b6052ec4
Taisei2 <tnbox3588git@gmail.com>
2026-04-11 21:01:29 +0900

    style: TodoItemのタスクを中央揃えに変更

--- src/components/features/TodoItem.tsx
+++ src/components/features/TodoItem.tsx
@@ -2,8 +2,8 @@ const TodoItem: React.FC = () => {
   return (
     <li className="list-row">
       <div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
-      <div className="list-col-grow">
-        <div className="text-xs uppercase font-semibold opacity-60">
+      <div className="flex justify-center items-center list-col-grow">
+        <div className="text-xs uppercase font-semibold">
           Remaining Reason
         </div>
       </div>
