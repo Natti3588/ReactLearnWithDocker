cd9ce9a9d30d1dadbd5b21920c89afe43b9d1468
Taisei2 <tnbox3588git@gmail.com>
2026-04-12 16:34:54 +0900

    style: biomeフォーマットを適用

--- src/components/features/TodoItem.tsx
+++ src/components/features/TodoItem.tsx
@@ -1,11 +1,17 @@
-const TodoItem: React.FC = () => {
+import type { Todo } from "./TodoList";
+
+type TodoItemProps = {
+  todo: Todo;
+  toggleTodo: (id: number) => void; // 引数に数値(id)を受け取り、何も返さない関数
+  deleteTodo: (id: number) => void;
+};
+
+const TodoItem: React.FC<TodoItemProps> = ({ todo, toggleTodo, deleteTodo }) => {
   return (
     <li className="list-row">
       <div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
       <div className="flex justify-center items-center list-col-grow">
-        <div className="text-xs uppercase font-semibold">
-          Remaining Reason
-        </div>
+        <div className="text-xs uppercase font-semibold">Remaining Reason</div>
       </div>
     </li>
   );
