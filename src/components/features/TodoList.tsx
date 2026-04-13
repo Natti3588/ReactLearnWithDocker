d4eadc1baaca4ae208a2e8ba4139e575ed1002ba
Taisei2 <tnbox3588git@gmail.com>
2026-04-13 09:19:23 +0900

    feat: TodoListにaddTodo関数を追加

--- src/components/features/TodoList.tsx
+++ src/components/features/TodoList.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import type { Todo } from "../../types/todo"
+import type { Todo } from "../../types/todo";
+import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 
 const TodoList: React.FC = () => {
@@ -11,19 +12,26 @@ const TodoList: React.FC = () => {
     );
   };
 
+  const addTodo = (text: string) => {
+    setTodos([...todos, { id: 1, text: text, completed: false }]);
+  };
+
   const deleteTodo = (id: number) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
-    <ul className="list bg-base-100 rounded-box shadow-md mx-10">
-      <li className="p-4 pb-2 text-lg opacity-60 tracking-wide text-left items-center border-b border-base-300">
-        Todo一覧
-      </li>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
-      ))}
-    </ul>
+    <>
+      <ul className="list bg-base-100 rounded-box shadow-md mx-10">
+        <li className="p-4 pb-2 text-lg opacity-60 tracking-wide text-left items-center border-b border-base-300">
+          Todo一覧
+        </li>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+        ))}
+      </ul>
+      <TodoForm addTodo={addTodo} />
+    </>
   );
 };
 
