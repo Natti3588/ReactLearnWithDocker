d93650f1192042fb40b1920be4cd1b7176e7a201
Taisei2 <tnbox3588git@gmail.com>
2026-04-15 11:14:03 +0900

    refactor: Buttonコンポーネントを使用しないので、削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { useState } from "react";
 import TodoList from "./components/features/TodoList";
-import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
-import Button from "./components/ui/Button";
+import Header from "./components/layouts/Header";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
 
   return (
     <>
       <Header />
-      <Button onClick={handleClick}>123</Button>
       <TodoList />
       <Footer />
     </>
